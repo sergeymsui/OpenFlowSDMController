fa7386c99428f3b64590bcda61e3c436992b2e70
Sergey Malichenko <s.malichenko@yandex.ru>
2025-03-23 22:37:37 +0300

    feat: update os ken

--- tutorial-ken/controller.py
+++ tutorial-ken/controller.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+"""
+Ryu Tutorial Controller
+
+This controller allows OpenFlow datapaths to act as Ethernet Hubs. Using the
+tutorial you should convert this to a layer 2 learning switch.
+
+See the README for more...
+"""
+
 from os_ken.base.app_manager import OSKenApp
 from os_ken.controller import ofp_event
 from os_ken.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
@@ -17,66 +26,54 @@ class Controller(OSKenApp):
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def features_handler(self, ev):
-        """
+        '''
         Handshake: Features Request Response Handler
 
         Installs a low level (0) flow table modification that pushes packets to
         the controller. This acts as a rule for flow-table misses.
-        """
+        '''
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         match = parser.OFPMatch()
-        actions = [
-            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
-        ]
-        self.logger.info(
-            "Handshake taken place with {}".format(dpid_to_str(datapath.id))
-        )
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.logger.info("Handshake taken place with {}".format(dpid_to_str(datapath.id)))
         self.__add_flow(datapath, 0, match, actions)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
-        """
+        '''
         Packet In Event Handler
 
         Takes packets provided by the OpenFlow packet in event structure and
         floods them to all ports. This is the core functionality of the Ethernet
         Hub.
-        """
+        '''
         msg = ev.msg
         datapath = msg.datapath
         ofproto = msg.datapath.ofproto
         parser = msg.datapath.ofproto_parser
         dpid = msg.datapath.id
         pkt = packet.Packet(msg.data)
-        in_port = msg.match["in_port"]
+        in_port = msg.match['in_port']
         data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
         actions = [datapath.ofproto_parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-        out = parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=msg.buffer_id,
-            in_port=in_port,
-            actions=actions,
-            data=data,
-        )
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
         self.logger.info("Sending packet out")
         datapath.send_msg(out)
         return
 
     def __add_flow(self, datapath, priority, match, actions):
-        """
+        '''
         Install Flow Table Modification
 
         Takes a set of OpenFlow Actions and a OpenFlow Packet Match and creates
         the corresponding Flow-Mod. This is then installed to a given datapath
         at a given priority.
-        """
+        '''
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(
-            datapath=datapath, priority=priority, match=match, instructions=inst
-        )
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
         self.logger.info("Flow-Mod written to {}".format(dpid_to_str(datapath.id)))
         datapath.send_msg(mod)
